037bcf0bd347b77148ce97b9319f1ca014f5de3c
LilNoahChan <noahchan305@gmail.com>
2023-01-11 13:16:50 -0500

    rr

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -8,8 +8,10 @@ const URL = "https://bible-api.com/BOOK+CHAPTER:VERSE"
 async function getData(URL) {
     try {
         const response = await fetch(URL)
-        const data = await response.json() //makes data into JSON object we can use
-        document.getElementById("api-response").textContent = data.content;
+        const data = await response.json(); 
+        console.log(data.data);
+        console.log (response);
+        return data;
     } catch (error) {
         console.log(error);
     }
